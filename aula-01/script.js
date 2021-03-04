627584a4ed1b73c788e488ecab42b4a9ea02abb9
Pedro Henrique Vinhas <peuvinhas2@gmail.com>
2021-03-04 19:49:09 -0300

    Aula 01

---
+++ aula-01/script.js
@@ -0,0 +1,17 @@
+// Aprendendo a usar o DevTools no Browser (F12)
+
+alert('Olá, mundo!')
+
+// Transformando o background do Google em preto
+document.body.style.background = 'black'
+
+// Transformando a logo do Google em preto e branco
+document.querySelector('img#hplogo').style.filter = 'grayscale(100%)'
+
+// Fazer desaparecer a logo do Google
+document.querySelector('img#hplogo').style.visibility = 'hidden'
+
+// Mudar a manchete do g1
+document.querySelector('span.bstn-h1-title').innerText = 'Curso em vídeo de javascript é o melhor do yotube'
+
+ 
