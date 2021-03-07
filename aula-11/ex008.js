2e97ad55df0f91be91d91f285d1239e20ce59fa7
Pedro Henrique Vinhas <peuvinhas2@gmail.com>
2021-03-06 23:51:08 -0300

    Aula 11 - Condicionais

---
+++ aula-11/ex008.js
@@ -0,0 +1,8 @@
+var vel = 12
+console.log(`A velocidade do seu carro é ${vel} km/h`)
+
+if ( vel > 60) { // Condiçãos simples
+    console.log(`Você ultrapassou a velocidade permitida`)
+}
+
+console.log(`Dirija sempre usando cinto de segurança`)
